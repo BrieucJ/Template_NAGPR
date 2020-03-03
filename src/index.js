43153c113beecea460bafc1bb0429e0db453c886
Brieuc Jamain <brieucjamain@MacBook-Pro-de-Brieuc.local>
2020-03-03 03:24:21 +0100

    new endpoint

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import { setContext } from 'apollo-link-context'
 //ROUTER
 import { BrowserRouter } from 'react-router-dom'
 
-const httpLink = createHttpLink({uri: 'http://localhost:4000/graphql'})
+const httpLink = createHttpLink({uri: 'http://localhost:4000'})
 const authLink = setContext((_, { headers }) => {
     const token = localStorage.getItem('token')
     return {
