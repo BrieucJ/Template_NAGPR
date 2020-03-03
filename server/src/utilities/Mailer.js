772ec160b7b18a65e8cfaa998407170d421ac07d
Brieuc Jamain <brieucjamain@MacBook-Pro-de-Brieuc.local>
2020-03-03 19:10:42 +0100

    test

--- server/src/utilities/Mailer.js
+++ server/src/utilities/Mailer.js
@@ -1,14 +1,21 @@
 const nodemailer = require('nodemailer');
 
-console.log(process.env.MAILGUN_API_KEY)
-console.log(process.env.MAILGUN_DOMAIN)
+// var transporter = nodemailer.createTransport({
+//     service: 'Mailgun',
+//     auth: {
+//         api_key: process.env.MAILGUN_API_KEY,
+//         domain: process.env.MAILGUN_DOMAIN
+//     }
+//   });
 
-var transporter = nodemailer.createTransport({
-    service: 'Mailgun',
+  var transporter = nodemailer.createTransport({
+    host: process.env.MAILGUN_SMTP_SERVER,
+    port: Sprocess.env.MAILGUN_SMTP_PORT,
+    secure: false, // true for 465, false for other ports
     auth: {
-        api_key: process.env.MAILGUN_API_KEY,
-        domain: process.env.MAILGUN_DOMAIN
-    }
+      user: process.env.MAILGUN_SMTP_LOGIN,
+      pass: process.env.MAILGUN_SMTP_PASSWORD,
+    },
   });
 
 module.exports = {
